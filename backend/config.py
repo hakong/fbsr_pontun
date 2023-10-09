afb3e696be3cecaa0f5ccbd5ab2b31558802a442
Hákon Gíslason <hakon.gislason@gmail.com>
2023-10-09 22:02:23 +0000

    backend config: grab settings from env

--- backend/config.py
+++ backend/config.py
@@ -1,21 +1,30 @@
 import os
 import os.path
 
-# Fill out in local_config.py
-HOSTNAME = ''
-ADMIN_PASSWORD = '' 
+# Grab settings from environment variables
+HOSTNAME = os.environ.get('WEB_HOSTNAME', '')
+ADMIN_PASSWORD = os.environ.get('ADMIN_PASSWORD', '')
 
-SMTP_PASSWORD = ''
-SMTP_SERVER = 'smtp.gmail.com'
-EMAIL_FROM = ''
+SMTP_PASSWORD = os.environ.get('SMTP_PASSWORD', '')
+SMTP_SERVER = os.environ.get('SMTP_SERVER', 'smtp.gmail.com')
+EMAIL_FROM = os.environ.get('EMAIL_FROM', '')
 
 # Useful for development (when there is no reverse proxy in front)
 
 LISTING_FRONTEND_FOLDER = os.path.join(os.path.dirname(__file__), '..', 'frontend', 'build')
 ADMIN_FRONTEND_FOLDER   = os.path.join(os.path.dirname(__file__), '..', 'admin',    'build')
-DATABASE = dict(database='fbsr_pontun', user='fbsr_pontun', host='127.0.0.1')
+DATABASE = {
+    'database': os.environ.get('DB_NAME', 'fbsr_pontun'),
+    'user': os.environ.get('DB_USER', 'fbsr_pontun'),
+    'host': os.environ.get('DB_HOST', '127.0.0.1'),
+    'password': os.environ.get('DB_PASSWORD', '')  # Defaulting to empty string if not set
+}
 
-TEMPLATE_VARS = {'account': '', 'kt': '', 'receipt_email': ''}
+TEMPLATE_VARS = {
+    'account': os.environ.get('TEMPLATE_ACCOUNT', ''),
+    'kt': os.environ.get('TEMPLATE_KT', ''),
+    'receipt_email': os.environ.get('TEMPLATE_RECEIPT_EMAIL', '')
+}
 
 try:
     from .local_config import *
